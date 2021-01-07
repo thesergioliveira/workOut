89d58c921522103e3483d2e6de86db61748bc5e6
Sergio <sergioliver93@gmail.com>
2021-01-07 15:12:10 +0100

    final organization for Dirs

--- january/04-01/myindex.js
+++ january/04-01/myindex.js
@@ -81,14 +81,16 @@ console.log(sub(10, 4));
 
 // ++ += -- -= 
 
-let num1 = 1;
 //num1 = num1 + 1;
+let num1 = 1;
 num1 += 1;
 console.log(num1);
+
 //num2 = num2 -1; num2 -=1;
 let num2 = 2;
 num2 -= 1;
 console.log(num2);
+
 //Increment
 let w = 10;
 w++;
