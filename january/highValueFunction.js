59712aca7b1b9f047708db07d26192f2d422e7e4
thesergioliveira <thesergioliveira@gmail.com>
2021-01-24 18:56:29 +0100

    learning new arrays methods

---
+++ january/highValueFunction.js
@@ -0,0 +1,12 @@
+let vals = ["4", "8", "2", "1"];
+
+console.log(vals);
+
+/* function doubler(x) {
+  return x * 2;
+} */
+
+vals = vals.map((x) => x * 2);
+
+//vals = vals.map(doubler);
+console.log(vals);
