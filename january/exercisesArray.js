bff72869ecb717a7a9290db452536d2a04d119b3
thesergioliveira <thesergioliveira@gmail.com>
2021-01-26 20:43:15 +0100

    new exercises and their solution

--- january/exercisesArray.js
+++ january/exercisesArray.js
@@ -1,3 +1,4 @@
+console.log("-----isFirstSuperior-------");
 /* You will be given two extremely similar arrays, but exactly one of the items in an array will be valued slightly higher than its counterpart (which means that evaluating the value > the other value will return true).
 Create a function that returns whether the first array is slightly superior to that of the second.
 Worked Example */
@@ -15,6 +16,8 @@ isFirstSuperior([true, 10, "zebra"], [true, 10, "zebra"]) ➞ false */
 Both arrays will be the same length.
 All values and their counterparts will always be of the same data type.
 If the two arrays are the same, return false. */
+
+console.log("----ToCamelCase--------");
 
 /* Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). */
 //toCamelCase("The_Stealth_Warrior"); // returns "TheStealthWarrior"
@@ -41,18 +44,3 @@ function toCamelCase(str) {
 console.log(toCamelCase("The_Stealth_Warrior"));
 console.log(toCamelCase(""));
 console.log(toCamelCase("The_Stealth_Warrior"));
-
-/*let newStr = str.split("").map((item, index) => {
-    let finalStr = [];
-    if (item == "-" || item == "_") {
-      newStr[index +1].toUpperCase() ;
-      finalStr.push()
-    } else {
-      finalStr.push(item);
-    }
-    return finalStr.join("");
-  });
-}
-
-console.log(toCamelCase("the-stealth-warrior"));
- */
