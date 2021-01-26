efd401ec21a68cf28f79531eaa317153a8104efa
thesergioliveira <thesergioliveira@gmail.com>
2021-01-26 20:41:00 +0100

    new exercises and their solution

--- january/exercisesArray.js
+++ january/exercisesArray.js
@@ -15,3 +15,44 @@ isFirstSuperior([true, 10, "zebra"], [true, 10, "zebra"]) ➞ false */
 Both arrays will be the same length.
 All values and their counterparts will always be of the same data type.
 If the two arrays are the same, return false. */
+
+/* Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). */
+//toCamelCase("The_Stealth_Warrior"); // returns "TheStealthWarrior"
+//toCamelCase("the-stealth-warrior"); // returns "theStealthWarrior"
+
+function toCamelCase(str) {
+  let newStr;
+  if (str == "") {
+    newStr = "";
+  } else if (str.includes("-")) {
+    newStr = str
+      .split("-")
+      .map((word, i) => (i ? word[0].toUpperCase() + word.slice(1) : word))
+      .join("");
+  } else if (str.includes("_")) {
+    newStr = str
+      .split("_")
+      .map((word, i) => (i ? word[0].toUpperCase() + word.slice(1) : word))
+      .join("");
+  }
+  return newStr;
+}
+
+console.log(toCamelCase("The_Stealth_Warrior"));
+console.log(toCamelCase(""));
+console.log(toCamelCase("The_Stealth_Warrior"));
+
+/*let newStr = str.split("").map((item, index) => {
+    let finalStr = [];
+    if (item == "-" || item == "_") {
+      newStr[index +1].toUpperCase() ;
+      finalStr.push()
+    } else {
+      finalStr.push(item);
+    }
+    return finalStr.join("");
+  });
+}
+
+console.log(toCamelCase("the-stealth-warrior"));
+ */
