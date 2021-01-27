914ed39b44c4600b20bdd6ca5c309408386609c8
thesergioliveira <thesergioliveira@gmail.com>
2021-01-27 21:54:10 +0100

    New exercises and solutions

--- january/exercisesArray.js
+++ january/exercisesArray.js
@@ -79,3 +79,26 @@ function getDecimal(n) {
   return Number(finalOutput);
 }
 console.log(getDecimal(2.00001));
+
+console.log("----_ArrayDiff--------");
+
+/* Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+It should remove all values from list a, which are present in list b. */
+/* arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+arrayDiff([1,2,2,2,3],[2]) == [1,3] */
+
+const arrayDif = (array, arr) => {
+  let output = [];
+  let newArrar = array.map((item) => {
+    if (!arr.includes(item)) {
+      output.push(item);
+    }
+    return output;
+  });
+  return output;
+  // if (!arr.includes(item)) {
+  //  output.push(item);
+  // }
+};
+console.log(arrayDif([1, 2, 2, 2, 3], [2]));
