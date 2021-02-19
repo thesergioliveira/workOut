17939c9c05224ae4c24578e8843beb66129f59f7
thesergioliveira <thesergioliveira@gmail.com>
2021-02-20 00:06:42 +0100

    changes to files and directories

---
+++ edabitChallenges.js
@@ -0,0 +1,16 @@
+function primorial(num) {
+  let numbers = [];
+  let finalNumbers;
+  let i = 0;
+  do {
+    i++;
+    if (i == 1 || i == 2 || i == 3 || i == 5) {
+      numbers.push(i);
+    } else if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % i == 0) {
+      numbers.push(i);
+    }
+  } while (numbers.length <= num);
+  finalNumbers = numbers.reduce((acc, item) => (acc *= item), 1);
+  return finalNumbers;
+}
+console.log(primorial(2));
